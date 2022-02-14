c3587c460eb500bc8a79238c3790ac1a89dab76b
abellsk <abelleng4@gmail.com>
2022-02-14 19:42:14 +0800

    editted js to test api

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ $(document).ready(function () {
           //we use ${content} because -> content += content 
           //we want to add on previous content at the same time
           content = `${content}<tr id='${response[i]._id}'>
-          <td>${response[i].username}</td>
+          <td>${response[i].userName}</td>
           <td>${response[i].password}</td>
   
           <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' 
@@ -125,80 +125,4 @@ $(document).ready(function () {
   
   
     }
-  
-    //[STEP 10]: Create our update listener
-    //here we tap onto our previous table when we click on update
-    //this is a delegation feature of jquery
-    //because our content is dynamic in nature, we listen in on the main container which is "#contact-list". For each row we have a class .update to help us
-    $("#contact-list").on("click", ".update", function (e) {
-      e.preventDefault();
-      //update our update form values
-      let contactName = $(this).data("name");
-      let contactEmail = $(this).data("email");
-      let contactMsg = $(this).data("msg");
-      let contactId = $(this).data("id");
-      console.log($(this).data("msg"));
-  
-      //[STEP 11]: Load in our data from the selected row and add it to our update contact form 
-      $("#update-contact-name").val(contactName);
-      $("#update-contact-email").val(contactEmail);
-      $("#update-contact-msg").val(contactMsg);
-      $("#update-contact-id").val(contactId);
-      $("#update-contact-container").show();
-  
-    });//end contact-list listener for update function
-  
-  
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-  
-    //[STEP 12]: Here we load in our contact form data
-    //Update form listener
-    $("#update-contact-submit").on("click", function (e) {
-      e.preventDefault();
-      //retrieve all my update form values
-      let contactName = $("#update-contact-name").val();
-      let contactEmail = $("#update-contact-email").val();
-      let contactMsg = $("#update-contact-msg").val();
-      let contactId = $("#update-contact-id").val();
-  
-      console.log($("#update-contact-msg").val());
-      console.log(contactMsg);
-  
-      //[STEP 12a]: We call our update form function which makes an AJAX call to our RESTDB to update the selected information
-      updateForm(contactId, contactName, contactEmail, contactMsg);
-    });//end updatecontactform listener
-  
-    //[STEP 13]: function that makes an AJAX call and process it 
-    //UPDATE Based on the ID chosen
-    function updateForm(id, contactName, contactEmail, contactMsg) {
-      //@TODO create validation methods for id etc. 
-  
-      var jsondata = { "name": contactName, "email": contactEmail, "message": contactMsg };
-      var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://interactivedev-adbb.restdb.io/rest/contact/${id}`,//update based on the ID
-        "method": "PUT",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(jsondata)
-      }
-  
-      //[STEP 13a]: send our AJAX request and hide the update contact form
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        
-        $("#update-contact-container").fadeOut(5000);
-        //update our contacts table
-        getContacts();
-      });
-    }//end updateform function
-  
   })
