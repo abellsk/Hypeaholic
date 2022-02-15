35a2cbe769fb82923eb9779c11674fde4a36d5df
abellsk <abelleng4@gmail.com>
2022-02-16 05:30:56 +0800

    testing login authentication

--- script.js
+++ script.js
@@ -84,64 +84,72 @@ $(document).ready(function () {
       //RESTDb/NoSql always adds in a unique id for each data, we tap on it to have our data and place it into our links 
   
       $.ajax(settings).done(function (response) {
-        
-        let content = "";
-  
-        for (var i = 0; i < response.length && i < limit; i++) {
-          //console.log(response[i]);
-          //[METHOD 1]
-          //let's run our loop and slowly append content
-          //we can use the normal string append += method
-          /*
-          content += "<tr><td>" + response[i].name + "</td>" +
-            "<td>" + response[i].email + "</td>" +
-            "<td>" + response[i].message + "</td>
-            "<td>Del</td><td>Update</td</tr>";
-          */
-  
-          //[METHOD 2]
-          //using our template literal method using backticks
-          //take note that we can't use += for template literal strings
-          //we use ${content} because -> content += content 
-          //we want to add on previous content at the same time
-          content = `${content}<tr id='${response[i]._id}'>
-          <td>${response[i].userName}</td>
-          <td>${response[i].password}</td>
-  
-          <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' 
-  
-          data-id='${response[i]._id}' data-msg='${response[i].message}' data-name='${response[i].name}' data-email='${response[i].email}
-  
-          '>Update</a></td></tr>`;
-  
+        if (tempUser == userName && tempPass == passWord) {
+          window.location.href = 'home.html';
+        } 
+        else {
+          window.alert('CANNOT');
         }
-  
-        //[STEP 9]: Update our HTML content
-        //let's dump the content into our table body
-        $("#contact-list tbody").html(content);
-  
-        $("#total-contacts").html(response.length);
+
       });
   
   
     }
   })
-
-  //[STEP 0]: Make sure our document is A-OK
+/*
+//Login Authentication
+//[STEP 0]: Make sure our document is A-OKrediret to 
 $(document).ready(function () {
-  //what kind of interface we want at the start 
+
+//what kind of interface we want at the start 
   const APIKEY = "61fcfb383f215f310a37be8f";
-  getContacts();
+  getUser();
+  $("#update-contact-container").hide();
   $("#add-update-msg").hide();
 
   //[STEP 1]: Create our submit form listener
   $("#login-submit").on("click", function (e) {
     //prevent default action of the button 
     e.preventDefault();
+
     //[STEP 2]: let's retrieve form data
     //for now we assume all information is valid
     //you are to do your own data validation
-    let userName = $("#username").val();
-    let passWord = $("#password").val();
-  }
-}
+    let tempUser = $("#username").val();
+    let tempPass = $("#password").val();
+    
+    //[STEP 3]: get form values when user clicks on send
+    //Adapted from restdb api
+    let jsondata = {
+      "username": userName,
+      "password": passWord
+    }
+  })
+  //[STEP] 6
+  //let's create a function to allow you to retrieve all the information in your contacts
+  //by default we only retrieve 10 results
+  function getUser(limit = 10, all = true) {
+    //[STEP 7]: Create our AJAX settings
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://hypeaholic-dde8.restdb.io/rest/username",
+      "method": "GET", //[cher] we will use GET to retrieve info
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+    }
+    //[STEP 8]: Make our AJAX calls
+    $.ajax(settings).done(function (response) {
+      if (tempUser == userName && tempPass == passWord) {
+        window.location.href = 'home.html';
+      } 
+      else {
+        window.alert('CANNOT');
+      }
+    }
+  });
+});
+*/
