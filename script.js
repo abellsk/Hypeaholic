36e4df8a98a7634ea51f6058f7b1a4766c6b6107
abellsk <abelleng4@gmail.com>
2022-02-16 18:50:02 +0800

    added } removed extra document ready and moved it

--- script.js
+++ script.js
@@ -125,61 +125,47 @@ $(document).ready(function () {
   
   
     }
-  })
-/*
-//Login Authentication
-//[STEP 0]: Make sure our document is A-OKrediret to 
-$(document).ready(function () {
 
-//what kind of interface we want at the start 
-  const APIKEY = "61fcfb383f215f310a37be8f";
-  getUser();
-  $("#update-contact-container").hide();
-  $("#add-update-msg").hide();
+    //Login Authentication
+    $("#login-submit").on("click", function (e) {
+      //prevent default action of the button 
+      e.preventDefault();
 
-  //[STEP 1]: Create our submit form listener
-  $("#login-submit").on("click", function (e) {
-    //prevent default action of the button 
-    e.preventDefault();
-
-    //[STEP 2]: let's retrieve form data
-    //for now we assume all information is valid
-    //you are to do your own data validation
-    let tempUser = $("#username").val();
-    let tempPass = $("#password").val();
+      //[STEP 2]: let's retrieve form data
+      //for now we assume all information is valid
+      //you are to do your own data validation
+      var tempUser = $("#username").val();
+      var tempPass = $("#password").val();
+      
     
-    //[STEP 3]: get form values when user clicks on send
-    //Adapted from restdb api
-    let jsondata = {
-      "username": userName,
-      "password": passWord
-    }
-  })
-  //[STEP] 6
-  //let's create a function to allow you to retrieve all the information in your contacts
-  //by default we only retrieve 10 results
-  function getUser(limit = 10, all = true) {
-    //[STEP 7]: Create our AJAX settings
-    let settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://hypeaholic-dde8.restdb.io/rest/username",
-      "method": "GET", //[cher] we will use GET to retrieve info
-      "headers": {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache"
-      },
-    }
-    //[STEP 8]: Make our AJAX calls
-    $.ajax(settings).done(function (response) {
-      if (tempUser == userName && tempPass == passWord) {
-        window.location.href = 'home.html';
-      } 
-      else {
-        window.alert('CANNOT');
+    //[STEP] 6
+    //let's create a function to allow you to retrieve all the information in your contacts
+    //by default we only retrieve 10 results
+    function getUser(limit = 10, all = true) {
+      //[STEP 7]: Create our AJAX settings
+      let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://hypeaholic-dde8.restdb.io/rest/username",
+        "method": "GET", //[cher] we will use GET to retrieve info
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
+        },
       }
-    }
-  });
-});
-*/
+      //[STEP 8]: Make our AJAX calls
+      $.ajax(settings).done(function (response) {
+        console.log(response[0].username)
+        console.log(response[0].password)
+        if (tempUser == response[0].username && tempPass == response[0].password) {
+          window.location.href = 'home.html';
+        } 
+        else {
+          window.alert('CANNOT');
+        }
+      }
+    )};
+  })
+})
+
